b52d9dd67449200ad6cfd036d559b0e3ee427cae
Hà Hoàng Hảo <112957852+HoangHao1009@users.noreply.github.com>
2024-01-11 13:49:08 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name = 'chatbot',
+  version = '0.1',
+  author = 'Ha Hoang Hao',
+  packages = ['chatbot'],
+  description = 'seq2seq architecture chatbot',
+  install_requires = ['torch'],
+)
